1a7518c9d32caea470603cec101e386506deebc8
xpsa0720 <xpsa0720@gmail.com>
2025-08-12 13:47:42 +0900

    Add to comment

--- lib/screen_on_flutter.dart
+++ lib/screen_on_flutter.dart
@@ -1,4 +1,4 @@
-
+import 'package:screen_on_flutter/class/alarm_model.dart';
 import 'package:screen_on_flutter/class/status_class.dart';
 
 import 'screen_on_flutter_platform_interface.dart';
@@ -16,23 +16,38 @@ class ScreenOnFlutter {
     return ScreenOnFlutterPlatform.instance.getPlatformVersion();
   }
 
+  /// Requests the necessary permissions.
+  ///
+  /// This method requests both the notification permission
+  /// and the "draw over other apps" permission.
   Future<StatusBase> requestPermission() async {
     final result = await channel.requestPermission_methodChannel();
     return StatusBase.fromMap(result.toString());
   }
 
-  Future<StatusBase> startService() async {
-    final result = await channel.startService_methodChannel();
+  /// Starts the service.
+  ///
+  /// You must call [requestPermission] before invoking this method.
+  Future<StatusBase> startService({
+    required AlarmModel model,
+  }) async {
+    final result = await channel.startService_methodChannel(
+        model: AlarmModel(
+      content: model.content,
+      title: model.title,
+    ));
     channel.startListenScreenOn(routeCallback: routeCallback);
     return StatusBase.fromMap(result.toString());
   }
 
+  /// Stops the service.
   Future<StatusBase> endService() async {
     final result = await channel.endService_methodChannel();
     channel.cancelListenScreenOn();
     return StatusBase.fromMap(result.toString());
   }
 
+  /// The [moveToBack] method closes the screen that appears when the device is turned on.
   Future<StatusBase> moveToBack() async {
     final result = await channel.moveToBack_methodChannel();
     return StatusBase.fromMap(result.toString());
